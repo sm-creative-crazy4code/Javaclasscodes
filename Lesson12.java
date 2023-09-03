db3e747e8ef1bd1109ddf2bb13cab9dfd0bbf003
sm-creative-crazy4code <snehacreative2016@gmail.com>
2023-09-03 13:37:24 +0530

    new code added

--- Lesson12.java
+++ Lesson12.java
@@ -1,10 +1,16 @@
-import java.utils.Scanner;
+import java.util.Scanner;
 
 public class Lesson12{
 
    public static void main(String[] args){
 
     Scanner input = new Scanner(System.in);
+    int year = input.nextInt();
+     if(year%400==0 || (year % 100 != 0) && (year % 4 == 0) ){
+      System.out.println("year is a leap year");
+     }else{
+      System.out.println("year is not a leap year");  
+     }
 
 
     
--- Lesson13.java
+++ Lesson13.java
@@ -1,10 +1,30 @@
 // find chinese Zodiac sign
+import java.util.Scanner;
 
 
 public class Lesson13{
 
 
     public static void main(String[] args){
+     Scanner input = new Scanner(System.in);
+    System.out.println("Enter your birh year");
+     int year= input.nextInt();
+
+     int z= year%12;
+     switch (z) {
+       case 0:System.out.println("Rat");break;
+       case 1:System.out.println("Ox");break;
+       case 2:System.out.println("Tiger");break;
+       case 3:System.out.println("Rabbit");break;
+       case 4:System.out.println("Dragon");break;
+       case 5:System.out.println("Snake");break;
+       case 6:System.out.println("Horse");break;
+       case 7:System.out.println("Sheep");break;
+       case 8:System.out.println("Monkey");break;
+       case 9:System.out.println("Rooster");break;
+       case 10:System.out.println("Dog");break;
+       case 11:System.out.println("Pig");break;
+     }
 
 
         
